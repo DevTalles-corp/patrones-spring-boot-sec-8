3ee756e7f7f29c82a008dac42adb7b95f5f8216a
Gabriel Chaldu <gchaldu@MacBook-Pro-de-Gabriel.local>
2026-03-26 19:14:53 -0300

    clase 17

--- src/main/java/com/atlas/bank/atlas_bank/account/model/Account.java
+++ src/main/java/com/atlas/bank/atlas_bank/account/model/Account.java
@@ -1,19 +1,19 @@
 package com.atlas.bank.atlas_bank.account.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Account {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String accountNumber;
     private String ownerName;
